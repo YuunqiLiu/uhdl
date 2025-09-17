793c3decce2db56f991fecf60fc562db985edde2
YuunqiLiu <liuyuunqi@163.com>
2025-09-17 20:51:53 +0800

    add a test for VComponent

--- test/test_VComponent.py
+++ test/test_VComponent.py
@@ -1,3 +1,4 @@
+import unittest
 from uhdl import *
 
 
@@ -20,3 +21,17 @@ def test_VComp_Basic():
     inst = VCompBasic()
     inst.output_dir = 'test_build'
     inst.generate_verilog()
+
+
+class TestVComponent(unittest.TestCase):
+    def test_param_and_ports_reflection(self):
+        # Using direct file source
+        vc32 = VComponent(file='test/verilog/module1.v', top='module1', DATA_WIDTH=32)
+        self.assertEqual(vc32.din.attribute, UInt(32))
+        self.assertEqual(vc32.dout.attribute, UInt(32))
+        self.assertEqual(vc32.DATA_WIDTH.verilog_def, ["parameter DATA_WIDTH = 32'd32"])
+
+        vc64 = VComponent(file='test/verilog/module1.v', top='module1', DATA_WIDTH=64)
+        self.assertEqual(vc64.din.attribute, UInt(64))
+        self.assertEqual(vc64.dout.attribute, UInt(64))
+        self.assertEqual(vc64.DATA_WIDTH.verilog_def, ["parameter DATA_WIDTH = 32'd64"])
